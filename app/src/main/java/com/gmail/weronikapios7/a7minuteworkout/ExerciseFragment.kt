d33b2c409e0577230ac2605083aecb0827b9cd02
Weronika <weronikapios7@gmail.com>
2022-04-04 13:35:18 +0200

    exercise timer done

--- app/src/main/java/com/gmail/weronikapios7/a7minuteworkout/ExerciseFragment.kt
+++ app/src/main/java/com/gmail/weronikapios7/a7minuteworkout/ExerciseFragment.kt
@@ -11,8 +11,13 @@ import com.gmail.weronikapios7.a7minuteworkout.databinding.FragmentExerciseBindi
 
 class ExerciseFragment : Fragment() {
     private var binding: FragmentExerciseBinding? = null
-    private var restTimer: CountDownTimer? = null
+
+    private var exerciseProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var totalExerciseTime = 30
+
     private var restProgress = 0
+    private var restTimer: CountDownTimer? = null
     private var totalRestTime = 10
 
     override fun onCreateView(
@@ -25,11 +30,19 @@ class ExerciseFragment : Fragment() {
     }
 
     private fun setupRestView(){
-        resetTimer()
-        setResProgressBar()
+        resetRestTimer()
+        setRestProgressBar()
     }
 
-    private fun setResProgressBar(){
+    private fun setupExerciseView(){
+        binding?.flCountdownRest?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flCountdownExercise?.visibility = View.VISIBLE
+        resetExerciseTimer()
+        setExerciseProgressBar()
+    }
+
+    private fun setRestProgressBar(){
         binding?.progressBar?.progress = restProgress
 
         restTimer = object: CountDownTimer(10000, 1000){
@@ -40,14 +53,32 @@ class ExerciseFragment : Fragment() {
             }
 
             override fun onFinish() {
-                //TODO
+                setupExerciseView()
             }
 
         }.start()
 
     }
 
-    private fun resetTimer(){
+    private fun setExerciseProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        exerciseTimer = object: CountDownTimer(30000, 1000){
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = totalExerciseTime - exerciseProgress
+                binding?.tvTimerExercise?.text = (totalExerciseTime - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                //TODO show rest view
+            }
+
+        }.start()
+
+    }
+
+    private fun resetRestTimer(){
         if(restTimer !=null){
             restTimer?.cancel()
             restProgress = 0
@@ -55,12 +86,19 @@ class ExerciseFragment : Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        resetTimer()
-        binding = null
+    private fun resetExerciseTimer(){
+        if(exerciseTimer !=null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+            totalExerciseTime = 0
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        resetRestTimer()
+        resetExerciseTimer()
+        binding = null
+    }
 
 }
