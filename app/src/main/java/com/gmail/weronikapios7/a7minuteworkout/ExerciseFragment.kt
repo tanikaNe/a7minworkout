b32b9e2a1818718299c64e32b10c74791f533555
Weronika <weronikapios7@gmail.com>
2022-04-06 11:42:19 +0200

    add info about next exercise and text to speech

--- app/src/main/java/com/gmail/weronikapios7/a7minuteworkout/ExerciseFragment.kt
+++ app/src/main/java/com/gmail/weronikapios7/a7minuteworkout/ExerciseFragment.kt
@@ -2,14 +2,18 @@ package com.gmail.weronikapios7.a7minuteworkout
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.gmail.weronikapios7.a7minuteworkout.databinding.FragmentExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class ExerciseFragment : Fragment() {
+class ExerciseFragment : Fragment(), TextToSpeech.OnInitListener {
     private var binding: FragmentExerciseBinding? = null
 
     private var exerciseProgress = 0
@@ -23,34 +27,47 @@ class ExerciseFragment : Fragment() {
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
+    private var tts: TextToSpeech? = null
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentExerciseBinding.inflate(inflater, container, false)
-        setupRestView()
         exerciseList = Constants.defaultExerciseList()
+        tts = TextToSpeech(context, this)
+        setupRestView()
         return binding?.root
     }
 
+
     private fun setupRestView(){
+        resetRestTimer()
+        setRestProgressBar()
         binding?.flCountdownRest?.visibility = View.VISIBLE
         binding?.tvTitle?.visibility = View.VISIBLE
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flCountdownExercise?.visibility = View.INVISIBLE
         binding?.ivExercise?.visibility = View.INVISIBLE
-        resetRestTimer()
-        setRestProgressBar()
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvNextExercise?.visibility = View.VISIBLE
+        binding?.tvNextExercise?.text = exerciseList!![currentExercisePosition + 1].getName()
+
     }
 
     private fun setupExerciseView(){
+        resetExerciseTimer()
+        setExerciseProgressBar()
+        speakOut(exerciseList!![currentExercisePosition].getName())
+
         binding?.flCountdownRest?.visibility = View.INVISIBLE
         binding?.tvTitle?.visibility = View.INVISIBLE
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flCountdownExercise?.visibility = View.VISIBLE
         binding?.ivExercise?.visibility = View.VISIBLE
-        resetExerciseTimer()
-        setExerciseProgressBar()
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvNextExercise?.visibility = View.INVISIBLE
 
         binding?.ivExercise?.setImageResource(exerciseList!![currentExercisePosition].getImage())
         binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
@@ -101,7 +118,7 @@ class ExerciseFragment : Fragment() {
         if(restTimer !=null){
             restTimer?.cancel()
             restProgress = 0
-            totalRestTime = 0
+            totalRestTime = 10
         }
     }
 
@@ -109,7 +126,7 @@ class ExerciseFragment : Fragment() {
         if(exerciseTimer !=null){
             exerciseTimer?.cancel()
             exerciseProgress = 0
-            totalExerciseTime = 0
+            totalExerciseTime = 30
         }
     }
 
@@ -118,6 +135,28 @@ class ExerciseFragment : Fragment() {
         resetRestTimer()
         resetExerciseTimer()
         binding = null
+
+        //show down text to speech on destroy
+        if(tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts?.setLanguage(Locale.ENGLISH)
+
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Toast.makeText(context, "Language is not supported", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(context, "Initialization Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun speakOut(text: String){
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
 }
